2bbe847b838d89fb074b768d05133f7e6be9c009
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2024-07-03 21:45:48 +0600

    refactor hard coded admin routes

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -6,33 +6,34 @@ import {
 } from "@ant-design/icons";
 import { Children, createElement } from "react";
 import { NavLink, Outlet } from "react-router-dom";
+import { adminSideBarItems } from "../../routes/admin.routes";
 
 const { Header, Content, Footer, Sider } = Layout;
 
-const items: MenuProps["items"] = [
-  {
-    key: "Dashboard",
-    label: <NavLink to="/admin/dashboard">Dashboard</NavLink>,
-  },
-  {
-    key: "User Management",
-    label: "User Management",
-    children: [
-      {
-        key: "Create Admin",
-        label: <NavLink to="/admin/create-admin">Create Admin</NavLink>,
-      },
-      {
-        key: "Create Faculty",
-        label: <NavLink to="/admin/create-faculty">Create Faculty</NavLink>,
-      },
-      {
-        key: "Create Student",
-        label: <NavLink to="/admin/create-student">Create Student</NavLink>,
-      },
-    ],
-  },
-];
+// const items: MenuProps["items"] = [
+//   {
+//     key: "Dashboard",
+//     label: <NavLink to="/admin/dashboard">Dashboard</NavLink>,
+//   },
+//   {
+//     key: "User Management",
+//     label: "User Management",
+//     children: [
+//       {
+//         key: "Create Admin",
+//         label: <NavLink to="/admin/create-admin">Create Admin</NavLink>,
+//       },
+//       {
+//         key: "Create Faculty",
+//         label: <NavLink to="/admin/create-faculty">Create Faculty</NavLink>,
+//       },
+//       {
+//         key: "Create Student",
+//         label: <NavLink to="/admin/create-student">Create Student</NavLink>,
+//       },
+//     ],
+//   },
+// ];
 
 const MainLayout = () => {
   return (
@@ -62,7 +63,7 @@ const MainLayout = () => {
           theme="dark"
           mode="inline"
           defaultSelectedKeys={["4"]}
-          items={items}
+          items={adminSideBarItems}
         />
       </Sider>
       <Layout>
--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,12 +1,24 @@
+import { ReactNode } from "react";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
+import { NavLink } from "react-router-dom";
 
-const adminPaths2 = [
+type TRoute = {
+  path: string;
+  element: ReactNode;
+};
+type TSideBarItem = {
+  key: string;
+  label: ReactNode;
+  children?: TSideBarItem[];
+};
+
+const adminPaths = [
   {
     name: "Dashboard",
-    path: "/admin/dashboard",
+    path: "dashboard",
     element: <AdminDashboard></AdminDashboard>,
   },
   {
@@ -14,38 +26,81 @@ const adminPaths2 = [
     children: [
       {
         name: "Create Admin",
-        path: "/admin/create-admin",
+        path: "create-admin",
         element: <CreateAdmin></CreateAdmin>,
       },
       {
         name: "Create Faculty",
-        path: "/admin/create-faculty",
+        path: "create-faculty",
         element: <CreateFaculty></CreateFaculty>,
       },
       {
         name: "Create Student",
-        path: "/admin/create-student",
+        path: "create-student",
         element: <CreateStudent></CreateStudent>,
       },
     ],
   },
 ];
 
-export const adminPaths = [
-  {
-    path: "dashboard",
-    element: <AdminDashboard></AdminDashboard>,
+export const adminSideBarItems = adminPaths.reduce(
+  (acc: TSideBarItem[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+        })),
+      });
+    }
+    return acc;
   },
-  {
-    path: "create-student",
-    element: <CreateStudent></CreateStudent>,
-  },
-  {
-    path: "create-admin",
-    element: <CreateAdmin></CreateAdmin>,
-  },
-  {
-    path: "create-faculty",
-    element: <CreateFaculty></CreateFaculty>,
-  },
-];
+  []
+);
+
+//* Programatical Way
+export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+  if (item.path && item.element) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
+  if (item.children) {
+    item.children.forEach((child) => {
+      acc.push({
+        path: child.path,
+        element: child.element,
+      });
+    });
+  }
+  return acc;
+}, []);
+
+//! Hard coded way
+// export const adminPaths = [
+//   {
+//     path: "dashboard",
+//     element: <AdminDashboard></AdminDashboard>,
+//   },
+//   {
+//     path: "create-student",
+//     element: <CreateStudent></CreateStudent>,
+//   },
+//   {
+//     path: "create-admin",
+//     element: <CreateAdmin></CreateAdmin>,
+//   },
+//   {
+//     path: "create-faculty",
+//     element: <CreateFaculty></CreateFaculty>,
+//   },
+// ];
